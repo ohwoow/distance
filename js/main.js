ba3f05f8e90cf0eb05a0a96a650474ead4588492
Даня <diman100l@mail.ru>
2020-06-05 17:20:38 +0500

    maybe final

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const menuBtn = document.querySelector('.ham-menu');
+const menuSidebar = document.querySelector('.menu-sidebar');
+const overlay = document.querySelector('.overlay');
+const body = document.querySelector('body');
+
+
+menuBtn.addEventListener('click', () => {
+    menuSidebar.classList.toggle('show');
+    overlay.classList.toggle('active'); 
+});
+
+overlay.addEventListener('click', () => {
+    menuSidebar.classList.remove('show');
+    overlay.classList.remove('active'); 
+});
