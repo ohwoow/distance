bb13dc5495f85bc458e2b1903f53c9b1d7b24d36
Даня <diman100l@mail.ru>
2020-06-06 12:20:05 +0500

    maybe final

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ const overlay = document.querySelector('.overlay');
 const body = document.querySelector('body');
 
 
+
 menuBtn.addEventListener('click', () => {
     menuSidebar.classList.toggle('show');
     overlay.classList.toggle('active'); 
+    body.classList.toggle('no-scroll');
 });
 
 overlay.addEventListener('click', () => {
